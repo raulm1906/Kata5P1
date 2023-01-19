70f8e28547d17dcabef221ecd90faa75618e4e17
Raúl <Ral@Marrero>
2023-01-19 11:20:55 +0000

    Version 1

---
+++ src/kata5p1/Kata5P1.java
@@ -0,0 +1,39 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Raúl Marrero Marichal
+ */
+
+
+public class Kata5P1 {
+
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:KATA5.db";
+        String sql = "SELECT * FROM PEOPLE";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)){
+            
+            while(rs.next()) {
+                System.out.println(rs.getInt("id") + "\t" 
+                                   + rs.getString("name") + "\t"
+                                   + rs.getString("Apellidos") + "\t"
+                                   + rs.getString("Departamento") + "\t");
+            }
+            
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        
+        
+    }
+    
+}
